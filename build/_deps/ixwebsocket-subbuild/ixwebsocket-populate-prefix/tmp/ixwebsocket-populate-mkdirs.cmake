12af7b0e5567b75da3600bdfca0929768e1b571a
conghieeu <doanconghieu2k3@gmail.com>
2026-04-28 21:48:46 +0700

    chore: sync project dependencies and rebuild build artifacts

---
+++ build/_deps/ixwebsocket-subbuild/ixwebsocket-populate-prefix/tmp/ixwebsocket-populate-mkdirs.cmake
@@ -0,0 +1,27 @@
+# Distributed under the OSI-approved BSD 3-Clause License.  See accompanying
+# file LICENSE.rst or https://cmake.org/licensing for details.
+
+cmake_minimum_required(VERSION ${CMAKE_VERSION}) # this file comes with cmake
+
+# If CMAKE_DISABLE_SOURCE_CHANGES is set to true and the source directory is an
+# existing directory in our source tree, calling file(MAKE_DIRECTORY) on it
+# would cause a fatal error, even though it would be a no-op.
+if(NOT EXISTS "D:/cpp-webview/cpp-webview-T02/build/_deps/ixwebsocket-src")
+  file(MAKE_DIRECTORY "D:/cpp-webview/cpp-webview-T02/build/_deps/ixwebsocket-src")
+endif()
+file(MAKE_DIRECTORY
+  "D:/cpp-webview/cpp-webview-T02/build/_deps/ixwebsocket-build"
+  "D:/cpp-webview/cpp-webview-T02/build/_deps/ixwebsocket-subbuild/ixwebsocket-populate-prefix"
+  "D:/cpp-webview/cpp-webview-T02/build/_deps/ixwebsocket-subbuild/ixwebsocket-populate-prefix/tmp"
+  "D:/cpp-webview/cpp-webview-T02/build/_deps/ixwebsocket-subbuild/ixwebsocket-populate-prefix/src/ixwebsocket-populate-stamp"
+  "D:/cpp-webview/cpp-webview-T02/build/_deps/ixwebsocket-subbuild/ixwebsocket-populate-prefix/src"
+  "D:/cpp-webview/cpp-webview-T02/build/_deps/ixwebsocket-subbuild/ixwebsocket-populate-prefix/src/ixwebsocket-populate-stamp"
+)
+
+set(configSubDirs Debug)
+foreach(subDir IN LISTS configSubDirs)
+    file(MAKE_DIRECTORY "D:/cpp-webview/cpp-webview-T02/build/_deps/ixwebsocket-subbuild/ixwebsocket-populate-prefix/src/ixwebsocket-populate-stamp/${subDir}")
+endforeach()
+if(cfgdir)
+  file(MAKE_DIRECTORY "D:/cpp-webview/cpp-webview-T02/build/_deps/ixwebsocket-subbuild/ixwebsocket-populate-prefix/src/ixwebsocket-populate-stamp${cfgdir}") # cfgdir has leading slash
+endif()
